b298e729482c52da58b88139216f9f9425b318ae
Alex Zhang <alexgzhang92@gmail.com>
2018-06-07 00:23:29 +0000

    Modified bin file and lib file to create better load dependencies

--- lib/top_ten_video_games.rb
+++ lib/top_ten_video_games.rb
@@ -1,5 +1,2 @@
-require "top_ten_video_games/version"
-
-module TopTenVideoGames
-  # Your code goes here...
-end
+require_relative "top_ten_video_games/version"
+require_relative "top_ten_video_games/cli"
