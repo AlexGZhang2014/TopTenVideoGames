6386e0d0e41c224445368ab52259786817cb4332
Alex Zhang <alexgzhang92@gmail.com>
2018-06-08 22:24:08 +0000

    Added helpful comments to game.rb for planning

--- lib/top_ten_video_games/game.rb
+++ lib/top_ten_video_games/game.rb
@@ -3,10 +3,10 @@ require 'open-uri'
 
 class TopTenVideoGames::Game
   doc = Nokogiri::HTML(open(http://www.metacritic.com/browse/games/score/metascore/90day/all/filtered?view=detailed))
-  games = doc.css("li.product.has_small_image")
+  games = doc.css("li.product.has_small_image") #Should return all games
   
   def scrape
-    games_array = []
+    games_array = [] #This should hold all new Game instances
     games.each.with_index do |game, i|
       until i == 10
         game = Game.new
@@ -16,7 +16,7 @@ class TopTenVideoGames::Game
     games_array
   end
   
-  def initialize
+  def initialize #The nils will be replaced with appropriate scraping code
     self.name = nil
     self.release_date = nil
     self.rating = nil
