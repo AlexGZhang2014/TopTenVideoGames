e0bc9524e292443ac0ca4d112d394f504740f601
Alex Zhang <alexgzhang92@gmail.com>
2018-06-08 22:22:28 +0000

    Deleted GameScraper class b/c it was unnecessary and moved that code to Game class

--- lib/top_ten_video_games/game.rb
+++ lib/top_ten_video_games/game.rb
@@ -1,4 +1,21 @@
+require 'nokigiri'
+require 'open-uri'
+
 class TopTenVideoGames::Game
+  doc = Nokogiri::HTML(open(http://www.metacritic.com/browse/games/score/metascore/90day/all/filtered?view=detailed))
+  games = doc.css("li.product.has_small_image")
+  
+  def scrape
+    games_array = []
+    games.each.with_index do |game, i|
+      until i == 10
+        game = Game.new
+        games_array << game
+      end
+    end
+    games_array
+  end
+  
   def initialize
     self.name = nil
     self.release_date = nil
