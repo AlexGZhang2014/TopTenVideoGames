1b32bc7edf87433285a7716e4f40c70b16f8965b
Alex Zhang <alexgzhang92@gmail.com>
2018-06-08 21:58:50 +0000

    Created new file game.rb with new object class Game and its #initialize method

---
+++ lib/top_ten_video_games/game.rb
@@ -0,0 +1,12 @@
+class TopTenVideoGames::Game
+  def initialize
+    self.name
+    self.release_date
+    self.rating
+    self.publisher
+    self.genre
+    self.user_score
+    self.platform
+  end
+  
+end
