699828dd7c3456ff7859d4c9f006e07d77fc2ff2
Alex Zhang <alexgzhang92@gmail.com>
2018-06-08 22:05:36 +0000

    Added open-uri to game_scraper.rb and defined doc variable

--- lib/top_ten_video_games/game_scraper.rb
+++ lib/top_ten_video_games/game_scraper.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class TopTenVideoGames::GameScraper
-  
+  doc = Nokogiri::HTML(open(http://www.metacritic.com/browse/games/score/metascore/90day/all/filtered?view=detailed))
 end
