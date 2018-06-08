02e933cb26405b96e9e9fc320dd834559188b339
Alex Zhang <alexgzhang92@gmail.com>
2018-06-08 23:57:40 +0000

    Added .strip to #scrape_games genre= method

--- lib/top_ten_video_games/game.rb
+++ lib/top_ten_video_games/game.rb
@@ -21,7 +21,7 @@ class TopTenVideoGames::Game
         game.release_date = games.css("div.more_stats li.release_date span.data").text
         game.rating = games.css("div.more_stats li.maturity_rating span.data").text
         game.publisher = games.css("div.more_stats li.publisher span.data").text
-        game.genre = games.css("div.more_stats li.genre span.data").text
+        game.genre = games.css("div.more_stats li.genre span.data").text.strip
         game.user_score = games.css("div.more_stats li.product_avguserscore span.data").text
         game.platform = games.css("div.more_stats li.platform_list span.data a").text
         self.all << game
