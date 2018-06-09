a349e15aa4e34c446203a6a6f5eecbc9013a2352
Alex Zhang <alexgzhang92@gmail.com>
2018-06-09 03:33:57 +0000

    Modified Game's methods to see if they work using the interface

--- lib/top_ten_video_games/game.rb
+++ lib/top_ten_video_games/game.rb
@@ -11,22 +11,19 @@ class Game
   
   def self.scrape_games
     doc = Nokogiri::HTML(open("https://www.ranker.com/list/most-popular-video-games-today/ranker-games"))
-    puts "#{doc}"
-    doc
-    #games = doc.css("h2.listItem") #Should return all games
-    #games.each do |game|
-      #new_game = Game.new
-      #new_game.name = games.css("a.listItem__title").text
-      #new_game.url = games.css("a.listItem__title").attr("href")
-      #new_game.release_year = games.css("span.listItem__properties").text
-      #new_game.rank = games.css("strong.listItem__rank").text
-      #new_game.description = games.css("span.listItem__wiki").text
-      #Game.all << new_game if new_game.rank.to_i < 11
-    #end
+    games = doc.css("h2.listItem") #Proven to return all games
+    new_game = self.new
+    new_game.name = games.css("a.listItem__title").text
+    new_game.url = games.css("a.listItem__title").attr("href").text
+    new_game.release_year = games.css("span.listItem__properties").text
+    new_game.rank = games.css("strong.listItem__rank").text
+    new_game.description = games.css("span.listItem__wiki").text
+    self.all << new_game
   end
   
   def self.list
-    Game.scrape_games
+    self.scrape_games
+    puts "#{self.all}"
     #self.all.each.with_index do |game, i|
       #puts "#{game.rank}. #{game.name} - Release Date: #{game.release_year} - #Description: #{game.description}"
     #end
