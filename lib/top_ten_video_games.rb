7c2092af66db54ec3a4102b7ceab2b5990601922
Alex Zhang <alexgzhang92@gmail.com>
2018-06-09 01:29:48 +0000

    Re-organized bin/console, added config/environment.rb, and bundle install

--- lib/top_ten_video_games.rb
+++ lib/top_ten_video_games.rb
@@ -1,7 +1,3 @@
-require "open-uri"
-require "nokogiri"
-require 'openssl'
-
 require_relative "top_ten_video_games/version"
 require_relative "top_ten_video_games/game"
 require_relative "top_ten_video_games/cli"
