f080a5c022c010b7b7413341a0a03e6605e74f96
Alex Zhang <alexgzhang92@gmail.com>
2018-06-09 21:17:13 +0000

    Modified #specific_game to use more efficient if/else statements that display Game instance properties

--- lib/top_ten_video_games/cli.rb
+++ lib/top_ten_video_games/cli.rb
@@ -15,55 +15,12 @@ class GameCLI
     input = nil
     until input == "exit"
       input = gets.strip
-      if input == "1"
-        puts Game.all[0].release_date
-        puts Game.all[0].description
-        puts Game.all[0].url
-        puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
-      elsif input == "2"
-        puts Game.all[1].release_date
-        puts Game.all[1].description
-        puts Game.all[1].url
-        puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
-      elsif input == "3"
-        puts Game.all[2].release_date
-        puts Game.all[2].description
-        puts Game.all[2].url
-        puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
-      elsif input == "4"
-        puts Game.all[3].release_date
-        puts Game.all[3].description
-        puts Game.all[3].url
-        puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
-      elsif input == "5"
-        puts Game.all[4].release_date
-        puts Game.all[4].description
-        puts Game.all[4].url
-        puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
-      elsif input == "6"
-        puts Game.all[5].release_date
-        puts Game.all[5].description
-        puts Game.all[5].url
-        puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
-      elsif input == "7"
-        puts Game.all[6].release_date
-        puts Game.all[6].description
-        puts Game.all[6].url
-        puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
-      elsif input == "8"
-        puts Game.all[7].release_date
-        puts Game.all[7].description
-        puts Game.all[7].url
-        puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
-      elsif input == "9"
-        puts Game.all[8].release_date
-        puts Game.all[8].description
-        puts Game.all[8].url
-        puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
-      elsif input == "10"
-        puts Game.all[9].release_date
-        puts Game.all[9].description
-        puts Game.all[9].url
+      number = input.to_i
+      if number > 0 && number < 11
+        puts "#{Game.all[number-1].name}"
+        puts "Release Date: #{Game.all[number-1].release_date}"
+        puts "Description: #{Game.all[number-1].description}"
+        puts "To see more in-depth information, go to this webpage: #{Game.all[number-1].url}"
         puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
       elsif input == "exit"
         puts "Thank you for using this app. Goodbye!"
