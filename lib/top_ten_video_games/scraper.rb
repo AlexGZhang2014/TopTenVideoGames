83ebd35ec8f8b60483226306c30d0cae3ecfccd4
Alex Zhang <alexgzhang92@gmail.com>
2018-06-09 02:44:54 +0000

    Modified Scraper class to not use an until loop

--- lib/top_ten_video_games/scraper.rb
+++ lib/top_ten_video_games/scraper.rb
@@ -2,16 +2,14 @@ class Scraper
   def self.scrape_games
     doc = Nokogiri::HTML(open("https://www.ranker.com/list/most-popular-video-games-today/ranker-games"))
     games = doc.css("h2.listItem") #Should return all games
-    games.each.with_index do |game, i|
-      until i == 10
-        game = Game.new
-        game.name = games.css("a.listItem__title").text
-        game.url = games.css("a.listItem__title").attr("href")
-        game.release_year = games.css("span.listItem__properties").text
-        game.rank = games.css("strong.listItem__rank").text
-        game.description = games.css("span.listItem__wiki").text
-        Game.all << game
-      end
+    games.each do |game|
+      new_game = Game.new
+      new_gamegame.name = games.css("a.listItem__title").text
+      new_game.url = games.css("a.listItem__title").attr("href")
+      new_game.release_year = games.css("span.listItem__properties").text
+      new_game.rank = games.css("strong.listItem__rank").text
+      new_game.description = games.css("span.listItem__wiki").text
+      Game.all << new_game if new_game.rank < 11
     end
   end
 end
