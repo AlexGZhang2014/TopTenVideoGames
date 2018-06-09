aa9414bbb22c8fe16a049ccf8f6836aa671e3b1a
Alex Zhang <alexgzhang92@gmail.com>
2018-06-09 02:47:15 +0000

    Modified Scraper class to fix some errors

--- lib/top_ten_video_games/scraper.rb
+++ lib/top_ten_video_games/scraper.rb
@@ -4,12 +4,12 @@ class Scraper
     games = doc.css("h2.listItem") #Should return all games
     games.each do |game|
       new_game = Game.new
-      new_gamegame.name = games.css("a.listItem__title").text
+      new_game.name = games.css("a.listItem__title").text
       new_game.url = games.css("a.listItem__title").attr("href")
       new_game.release_year = games.css("span.listItem__properties").text
       new_game.rank = games.css("strong.listItem__rank").text
       new_game.description = games.css("span.listItem__wiki").text
-      Game.all << new_game if new_game.rank < 11
+      Game.all << new_game if new_game.rank.to_i < 11
     end
   end
 end
