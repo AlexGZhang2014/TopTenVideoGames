ff55ac15eaed3b6a575a5e98ea555796e1fdafde
Alex Zhang <alexgzhang92@gmail.com>
2018-06-09 21:00:21 +0000

    Modified #specific_game method to output release_date, description, and url properties

--- lib/top_ten_video_games/cli.rb
+++ lib/top_ten_video_games/cli.rb
@@ -17,42 +17,52 @@ class GameCLI
       input = gets.strip
       if input == "1"
         puts Game.all[0].release_date
+        puts Game.all[0].description
         puts Game.all[0].url
         puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
       elsif input == "2"
-        puts "Go to this webpage to view more info on this game."
+        puts Game.all[1].release_date
+        puts Game.all[1].description
         puts Game.all[1].url
         puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
       elsif input == "3"
-        puts "Go to this webpage to view more info on this game."
+        puts Game.all[2].release_date
+        puts Game.all[2].description
         puts Game.all[2].url
         puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
       elsif input == "4"
-        puts "Go to this webpage to view more info on this game."
+        puts Game.all[3].release_date
+        puts Game.all[3].description
         puts Game.all[3].url
         puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
       elsif input == "5"
-        puts "Go to this webpage to view more info on this game."
+        puts Game.all[4].release_date
+        puts Game.all[4].description
         puts Game.all[4].url
         puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
       elsif input == "6"
-        puts "Go to this webpage to view more info on this game."
+        puts Game.all[5].release_date
+        puts Game.all[5].description
         puts Game.all[5].url
         puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
       elsif input == "7"
-        puts "Go to this webpage to view more info on this game."
+        puts Game.all[6].release_date
+        puts Game.all[6].description
         puts Game.all[6].url
         puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
       elsif input == "8"
-        puts "Go to this webpage to view more info on this game."
+        puts Game.all[7].release_date
+        puts Game.all[7].description
         puts Game.all[7].url
         puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
       elsif input == "9"
-        puts "Go to this webpage to view more info on this game."
+        puts Game.all[8].release_date
+        puts Game.all[8].description
         puts Game.all[8].url
         puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
       elsif input == "10"
-        puts "Go to this webpage to view more info on this game."
+        puts Game.all[9].release_date
+        puts Game.all[9].description
         puts Game.all[9].url
         puts "To view more info on a different game, please enter a number 1-10. Otherwise, type \'exit\' when you are done."
       elsif input == "exit"
