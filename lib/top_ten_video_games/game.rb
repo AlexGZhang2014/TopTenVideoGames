860fd709f82408183317732e74667c447606f759
Alex Zhang <alexgzhang92@gmail.com>
2018-06-14 21:06:18 +0000

    Remove unnecessary methods #game_scraper and #list

--- lib/top_ten_video_games/game.rb
+++ lib/top_ten_video_games/game.rb
@@ -1,36 +1,12 @@
-require_relative "../../config/environment.rb"
-
-class Game
+# 1. Create a Scraper class that's responsible for just scraping - done
+# 2. All `puts` statements should go in the CLI class - done
+# 3. Talk about module namespacing - done
+class TopTenVideoGames::Game
   attr_accessor :name, :rank, :release_date, :description, :url
   
   @@all = []
   
   def self.all
     @@all
-  end
-  
-  def self.scrape_games
-    doc = Nokogiri::HTML(open("https://www.ranker.com/list/most-popular-video-games-today/ranker-games"))
-    games = doc.css("h2.listItem") #Proven to return all 50 games
-    index = 0
-    games.each do |game|
-      while index < 10
-        new_game = self.new
-        new_game.name = games.css("a.listItem__title")[index].text
-        new_game.url = games.css("a.listItem__title")[index].attr("href")
-        new_game.release_date = games.css("span.listItem__properties")[index].text
-        new_game.rank = games.css("strong.listItem__rank")[index].text
-        new_game.description = games.css("span.listItem__wiki")[index].text
-        self.all << new_game
-        index += 1
-      end
-    end
-  end
-  
-  def self.list
-    self.scrape_games
-    self.all.each do |game|
-      puts "#{game.rank}. #{game.name}"
-    end
   end
 end
